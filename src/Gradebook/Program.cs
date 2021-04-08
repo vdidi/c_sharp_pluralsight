54d5ff42091b746012b202fd63509310c8f315da
vdidi <vitor_hugo_di@hotmail.com>
2021-04-08 20:12:48 -0300

    feat: add arguments in the command line

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -6,7 +6,7 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hello {args[0]}!");
         }
     }
 }
