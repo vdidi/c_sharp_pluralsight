3d31c5efe4d25b2cedeca37f0faa51a329fdbca3
vdidi <vitor_hugo_di@hotmail.com>
2021-04-09 08:06:47 -0300

    feat: adding formating and dividing numbers

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -7,18 +7,16 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {   
-            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
             List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
             var result = 0.0;
 
-
             foreach(var number in grades) {
                 result += number;
             }
-
-            Console.WriteLine(result);
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
 
             if(args.Length > 0) {
                 Console.WriteLine($"Hello {args[0]}!");
