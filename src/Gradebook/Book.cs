6f014bf9b0ba40a970c452db40f6fd3bd5582461
vdidi <vitor_hugo_di@hotmail.com>
2021-04-10 19:48:42 -0300

    refactor: encapsulating statistics method

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Gradebook {
@@ -11,6 +12,23 @@ namespace Gradebook {
 
     }
 
+    public void ShowStatistics() {
+      var result = 0.0;
+      var highGrade = double.MinValue;
+      var lowGrade = double.MaxValue;
+
+      foreach(var number in grades) {
+          lowGrade = Math.Min(number, lowGrade);
+          highGrade = Math.Max(number, highGrade);
+          result += number;
+      }
+      result /= grades.Count;
+      Console.WriteLine($"The lowest grade is {lowGrade}");
+      Console.WriteLine($"The lowest grade is {highGrade}");
+      Console.WriteLine($"The average grade is {result:N1}");
+    }
+
     List<double> grades;
+
   }
 }
--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -10,23 +9,9 @@ namespace Gradebook
 
             var book = new Book();
             book.AddGrade(89.1);
-
-            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
-
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-
-            foreach(var number in grades) {
-                lowGrade = Math.Min(number, lowGrade);
-                highGrade = Math.Min(number, highGrade);
-                result += number;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"The lowest grade is {lowGrade}");
-            Console.WriteLine($"The lowest grade is {highGrade}");
-            Console.WriteLine($"The average grade is {result:N1}");
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
         }
     }
 }
