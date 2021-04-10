0b2b054c57f23259bbfb81babf0c1a1012affb5a
vdidi <vitor_hugo_di@hotmail.com>
2021-04-10 19:42:41 -0300

    feat: adding statistics

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -15,19 +15,18 @@ namespace Gradebook
             grades.Add(56.1);
 
             var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
 
             foreach(var number in grades) {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Min(number, highGrade);
                 result += number;
             }
             result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The lowest grade is {highGrade}");
             Console.WriteLine($"The average grade is {result:N1}");
-
-            if(args.Length > 0) {
-                Console.WriteLine($"Hello {args[0]}!");
-            }
-            else {
-                Console.WriteLine("Hello!");
-            }
         }
     }
 }
