4d9f053a95157c2c566c225102f233aeb7aab1ef
vdidi <vitor_hugo_di@hotmail.com>
2021-04-13 08:37:14 -0300

    feat: chaining constructors

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -4,16 +4,20 @@ using System.Collections.Generic;
 namespace Gradebook {
 
   public class NamedObject {
-      public string Name
-      {
-        get;
-        set;
-      }
+
+    public NamedObject(string name) {
+      Name = name;
+    } 
+    public string Name
+    {
+      get;
+      set;
+    }
   }
 
   public class Book : NamedObject {
 
-    public Book(string name) {
+    public Book(string name): base(name) {
       grades = new List<double>();
       Name = name;
     }
